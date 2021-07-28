1e7f2679d9bcff436d5d0e1104545c42ad9929d8
haroonsaeed82096 <30671721+haroonsaeed82096@users.noreply.github.com>
2021-07-28 22:19:50 +0200

    Special Attack

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      currentRound: 0,
     };
   },
   computed: {
@@ -16,16 +17,26 @@ const app = Vue.createApp({
     playerBarStyles() {
       return { width: this.playerHealth + "%" };
     },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    },
   },
   methods: {
     attackMonster() {
-      const attackValue = getRandomValue(5, 12);
-      this.monsterHealth = this.monsterHealth - attackValue;
+      this.currentRound++;
+      const attackValue = getRandomValue(8, 15);
+      this.playerHealth = this.playerHealth - attackValue;
       this.attackPlayer();
     },
     attackPlayer() {
       const attackValue = getRandomValue(8, 15);
       this.playerHealth = this.playerHealth - attackValue;
+    },
+    specialAttackMosnter() {
+      this.currentRound++;
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth = this.monsterHealth - attackValue;
+      this.attackPlayer();
     },
   },
 });
